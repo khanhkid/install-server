4820cc468c04862088b06e6922f1a11450056a63
khanhkid <sachlong112359@gmail.com>
2019-07-09 15:17:57 +0700

    Update install-php71.sh

--- install-php71.sh
+++ install-php71.sh
@@ -3,7 +3,7 @@ echo ">>> Installing PHP7.1-FPM"
 
 sudo apt-get install -y software-properties-common
 
-sudo apt install apt-transport-https lsb-release ca-certificates
+sudo apt install -y apt-transport-https lsb-release ca-certificates
 sudo wget -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
 sudo sh -c 'echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" > /etc/apt/sources.list.d/php.list'
 sudo apt update
