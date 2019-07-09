f446252474bc84ea12f79fd2e51798f4d1db67b4
KhanhVo <khanhvo@vinaresearch.net>
2019-07-09 16:36:01 +0700

    update install server bash script

--- main.sh
+++ main.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 sudo apt-get install -y git
 
-SCRIPT_PATH="/path/to"
-
-bash "$SCRIPT_PATH/install-mariadb.sh"
-
 wget -O - https://raw.githubusercontent.com/khanhkid/install-server/master/install-php71.sh | bash
 wget -O - https://raw.githubusercontent.com/khanhkid/install-server/master/install-nginx.sh | bash
 wget -O - https://raw.githubusercontent.com/khanhkid/install-server/master/install-mariadb.sh | bash
